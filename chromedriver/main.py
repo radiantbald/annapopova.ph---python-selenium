6399ece5bf14700896074991e323e6974de1de98
Radiant Bald <radiantbald@MacBook-Pro-Radiant.local>
2023-04-09 21:16:56 +0300

    TheFirst

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,13 +1,41 @@
-from selenium import webdriver
+# from selenium import webdriver
+from seleniumwire import webdriver
 import time
+import random
+from fake_useragent import UserAgent
+from proxy_auth_data import login, password
 
-url = 'https://annapopovaph.ru/'
-driver = webdriver.Chrome(executable_path='/Users/radiantbald/QA/Selenium/chromedriver/chromedriver')
+# url = 'https://annapopovaph.ru/'
+
+# user_agents_list = [
+#     'hello_world',
+#     'best_of_the_best',
+#     'python_today'
+# ]
+
+options = webdriver.ChromeOptions()
+
+# useragent = UserAgent()
+
+# options.add_argument(f'user-agent={random.choice(user_agents_list)}')
+# options.add_argument(f'user-agent={useragent.ie}')
+# options.add_argument('--proxy-server=138.128.91.63:8000')
+
+proxy_options = {
+    'proxy': {
+        'https': f'http//{login}:{password}@138.128.91.63:8000'
+    }
+}
+
+driver = webdriver.Chrome(executable_path = '/Users/radiantbald/QA/Selenium/chromedriver/chromedriver', 
+                          seleniumwire_options = proxy_options
+                          )
 
 try:
-    driver.get(url=url)
-    driver.save_screenshot('annapopova.png')
-    time.sleep(5)
+     # driver.get(url = 'https://www.whatismybrowser.com/detect/what-is-my-user-agent/')
+     # driver.save_screenshot('annapopova.png')
+     driver.get('https://2ip.ru')
+     time.sleep(20)
 except Exception as ex:
     print(ex)
 finally:
